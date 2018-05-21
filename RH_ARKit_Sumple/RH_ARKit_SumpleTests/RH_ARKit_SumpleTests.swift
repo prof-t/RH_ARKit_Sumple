b8aa5a102861e418633858b448f59807f9542866
prof-t <miniature-dachs@excite.co.jp>
2018-05-21 19:51:13 +0900

    [新機能]プロジェクト作成

---
+++ RH_ARKit_Sumple/RH_ARKit_SumpleTests/RH_ARKit_SumpleTests.swift
@@ -0,0 +1,36 @@
+//
+//  RH_ARKit_SumpleTests.swift
+//  RH_ARKit_SumpleTests
+//
+//  Created by 平山亮 on 2018/05/21.
+//  Copyright © 2018年 平山亮. All rights reserved.
+//
+
+import XCTest
+@testable import RH_ARKit_Sumple
+
+class RH_ARKit_SumpleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
